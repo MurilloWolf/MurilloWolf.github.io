992980e16a6434d314cc4ade3a9405128a0fb4b3
MurilloWolf <mwolfcavalheiro@gmail.com>
2019-09-05 17:29:24 -0300

    fix bugs in useApi

--- public/js/useApi.js
+++ public/js/useApi.js
@@ -1,20 +1,44 @@
-const avatarName = document.getElementById("avatar-name");
-const avatarDesc = document.getElementById("avatar-desc");
-const avatarFigure = document.getElementById("figure");
+function submitForm() {
+  const avatarName = document.getElementById("avatar-name");
+  const avatarDesc = document.getElementById("avatar-desc");
+  const avatarFigure = document.getElementById("figure");
+  const avatarImage = document.getElementById("avatar-image");
+  const repositorios = document.getElementById("rep-container");
+  const quantidadeRep = document.getElementById("qtd-rep");
+  const userName = document.getElementById("input-search").value;
+  const link = document.getElementsByTagName("a");
 
-axios({
-  method: "get", // metodo http
-  url: "https://api.github.com/users/vinicius-vasconcelos" //caminho e parametros da api
-})
-  .then(response => {
-    console.log(response);
-    avatarFigure.insertAdjacentHTML(
-      "afterbegin",
-      `<img class='image' src = ${response.data.avatar_url} alt= ${avatarName}/>`
-    );
-    avatarName.innerText = response.data.name;
-    avatarDesc.innerText = response.data.bio;
+  console.log(link);
+
+  repositorios.innerHTML = "";
+
+  axios({
+    method: "get", // metodo http
+    url: `https://api.github.com/users/${userName}/repos` //caminho e parametros da api
   })
-  .catch(error => {
-    console.log(error);
-  });
+    .then(response => {
+      console.log(response.data);
+      avatarImage.setAttribute("src", "" + response.data[0].owner.avatar_url);
+      link.setAttribute("href", response.data[0].owner.html_url);
+      avatarName.innerText = response.data[0].owner.login;
+      quantidadeRep.innerText = response.data.length;
+
+      response.data.map(item => {
+        let desc;
+        item.description == null ? (desc = "") : (desc = item.description);
+
+        repositoryHTML = ` 
+              <div class="rep-item">
+              <p class="rep-item-title">${item.name}</p>
+              <p>
+                ${desc}
+              </p>
+              </div>`;
+
+        repositorios.insertAdjacentHTML("afterbegin", repositoryHTML);
+      });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
